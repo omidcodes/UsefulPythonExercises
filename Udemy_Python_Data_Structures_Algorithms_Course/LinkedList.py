c4c31fd8f53f9053ed36b2920313bf76f93c3392
hashemzadeh <hashemzadeh@mahsan.co>
2024-02-11 15:18:10 +0330

    pop_list fixed

--- Udemy_Python_Data_Structures_Algorithms_Course/LinkedList.py
+++ Udemy_Python_Data_Structures_Algorithms_Course/LinkedList.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 class Node:
     
     def __init__(self, value) -> None:
@@ -12,9 +14,9 @@ class LinkedList:
         self.tail = node
         self.length = 1
 
-    def print_values(self) -> None:
+    def print_list(self) -> None:
 
-        print(f"self.length = {self.length}")
+        # print(f"self.length = {self.length}")
         
         ptr = self.head
 
@@ -55,22 +57,29 @@ class LinkedList:
             current_iteration = current_iteration.next         
 
         return before_iteration
+    
 
-    def pop(self):
+    def pop(self) -> Optional[Node]:
         if self.length == 0:
-            raise Exception("There is no item to pop")
+            return None
+            # raise Exception("There is no item to pop")
         
         if self.length == 1:
+            return_node = self.tail
             self.head = None
             self.tail = None
-            self.length = 0
-            return
 
-        one_before_the_last_node = self.__find_one_before_the_last_node()
-        one_before_the_last_node.next = None
-        self.tail = one_before_the_last_node
+        else:
+            one_before_the_last_node : Node = self.__find_one_before_the_last_node()
+            one_before_the_last_node.next = None
+
+            return_node = self.tail
+            
+            self.tail = one_before_the_last_node
 
         self.length -= 1
+
+        return return_node
 
     def prepend(self, value):
         """ Add to the beginning of the LinkedList"""
@@ -86,19 +95,25 @@ class LinkedList:
         
         self.length +=1
         
-    def pop_first(self):
+    def pop_first(self) -> Optional[Node]:
         
         if self.length == 0:
-            raise Exception("There is no item to pop")
+            return None
+            # raise Exception("There is no item to pop")
         elif self.length == 1:
+            return_node = self.head
             self.head.next = None
             self.head = None
+            self.tail = None
+            
         else:
+            return_node = self.head
             second_element = self.head.next
             self.head.next = None
             self.head = second_element
 
         self.length -=1
+        return return_node
 
     def __validate_index(self, index) -> None:
         if index < 0 or index > self.length - 1 :
@@ -208,37 +223,37 @@ print('Tail:', my_linked_list.tail.value)
 print('Length:', my_linked_list.length)
 
 
-my_linked_list.print_values()
+my_linked_list.print_list()
 
 print("Popping The last item ...")
 my_linked_list.pop()                                                                                     
 
-my_linked_list.print_values()
+my_linked_list.print_list()
 
 print("Prepending The item ...")
 my_linked_list.prepend(7)
-my_linked_list.print_values()
+my_linked_list.print_list()
 
 print("Popping The First item ...")
 my_linked_list.pop_first()
-my_linked_list.print_values()
+my_linked_list.print_list()
 
 print("getting only the item With Index = `2`")
 print(my_linked_list.get(2))
 
 print("`set_value` The item ...")
 my_linked_list.set_value(index=0, value=99)
-my_linked_list.print_values()
+my_linked_list.print_list()
 
 print("`insert` The item ...")
 my_linked_list.insert(index=3, value=40)
-my_linked_list.print_values()
+my_linked_list.print_list()
 
 
 print("`Removing` The item ...")
 my_linked_list.remove(index=3)
-my_linked_list.print_values()
+my_linked_list.print_list()
 
 print("`Reversing` The items ...")
 my_linked_list.reverse_items()
-my_linked_list.print_values()
+my_linked_list.print_list()
