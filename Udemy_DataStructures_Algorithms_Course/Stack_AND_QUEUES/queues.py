f6d6c7c1128eb55a6e11ff0ae480e54fa0c5118e
hashemzadeh <hashemzadeh@mahsan.co>
2024-02-17 14:05:51 +0330

    Queues added

--- Udemy_DataStructures_Algorithms_Course/Stack_AND_QUEUES/queues.py
+++ Udemy_DataStructures_Algorithms_Course/Stack_AND_QUEUES/queues.py
@@ -23,6 +23,23 @@ class Queue:
             self.last = new_node
         self.length += 1
 
+    def dequeue(self):
+        if self.length == 0:
+            return None
+
+        temp = self.first
+
+        if self.length == 1:
+            self.first = None
+            self.last = None
+
+        else:
+            self.first = self.first.next
+            temp.next = None
+
+        self.length -= 1
+        return temp
+
     def print_queue(self):
         node = self.first
         while node:
@@ -34,4 +51,19 @@ my_queue = Queue(1)
 my_queue.enqueue(2)
 my_queue.enqueue(3)
 my_queue.enqueue(4)
-my_queue.print_queue()
+
+print(my_queue.dequeue().value)
+print(my_queue.dequeue().value)
+print(my_queue.dequeue().value)
+print(my_queue.dequeue().value)
+# (0) Items - Returns None
+print(my_queue.dequeue())
+
+"""
+    EXPECTED OUTPUT:
+    ----------------
+    1
+    2
+    None
+
+"""
