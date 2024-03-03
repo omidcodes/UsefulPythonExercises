be8ec290b063a5b53f8f1467a8f79666f36e69b5
hashemzadeh <hashemzadeh@mahsan.co>
2024-03-03 11:47:55 +0330

    Summary Ranges added

--- LeetCodeExamples/228. Summary Ranges.py
+++ LeetCodeExamples/228. Summary Ranges.py
@@ -1,36 +1,36 @@
-# TODO : FIX me ...
+from typing import List
 
-# from typing import List
-#
-#
-# class Solution:
-#
-#     def summaryRanges(self, nums: List[int]) -> List[str]:
-#
-#         output = []
-#
-#         i = 0
-#         while True:
-#
-#             if i == len(nums) - 1:
-#                 # The last one
-#                 output.append(f"{nums[i]}")
-#                 break
-#
-#             for j in range(i, len(nums)):
-#                 if nums[j] != nums[j - 1] + 1:
-#                     if i == j - 1:
-#                         output.append(f"{nums[i]}")
-#                     else:
-#                         output.append(f"{nums[i]}->{nums[j]}")
-#
-#                     i = j + 1
-#
-#                     break
-#
-#         return output
-#
-#
-# nums = [0, 2, 3, 4, 6, 8, 9]
-#
-# print(Solution().summaryRanges(nums=nums))
+
+class Solution:
+
+    def summaryRanges(self, nums: List[int]) -> List[str]:
+
+        output = []
+
+        head_idx = 0
+
+        len_nums = len(nums)
+
+        while True:
+            ptr_idx = head_idx
+
+            if head_idx > len_nums - 1:
+                break
+
+            while ptr_idx != len_nums - 1 and nums[ptr_idx + 1] == nums[ptr_idx] + 1:
+                ptr_idx += 1
+
+            if ptr_idx == head_idx:
+                output.append(f"{nums[head_idx]}")
+            else:
+                output.append(f"{nums[head_idx]}->{nums[ptr_idx]}")
+
+            head_idx = ptr_idx + 1
+
+        return output
+
+
+nums = [0, 2, 3, 4, 6, 8, 9]
+# nums = [0, 1, 2, 4, 5, 7]
+
+print(Solution().summaryRanges(nums=nums))
