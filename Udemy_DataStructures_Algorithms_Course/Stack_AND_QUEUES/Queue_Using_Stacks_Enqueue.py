71a37a5594bbe391f2f78e23d7afd14591c66fc5
hashemzadeh <hashemzadeh@mahsan.co>
2024-03-03 16:06:03 +0330

    Queue Using Stacks

---
+++ Udemy_DataStructures_Algorithms_Course/Stack_AND_QUEUES/Queue_Using_Stacks_Enqueue.py
@@ -0,0 +1,45 @@
+class MyQueue:
+    def __init__(self):
+        self.stack1 = []
+        self.stack2 = []
+
+    def enqueue(self, value):
+
+        while self.stack1 != []:
+            popped_value = self.stack1.pop()
+            self.stack2.append(popped_value)
+
+        self.stack2.append(value)
+
+        while self.stack2 != []:
+            popped_value = self.stack2.pop()
+            self.stack1.append(popped_value)
+
+    def peek(self):
+        return self.stack1[-1]
+
+    def is_empty(self):
+        return len(self.stack1) == 0
+
+
+# Create a new queue
+q = MyQueue()
+
+# Enqueue some values
+q.enqueue(1)
+q.enqueue(2)
+q.enqueue(3)
+
+# Output the front of the queue
+print("Front of the queue:", q.peek())
+
+# Check if the queue is empty
+print("Is the queue empty?", q.is_empty())
+
+"""
+    EXPECTED OUTPUT:
+    ----------------
+    Front of the queue: 1
+    Is the queue empty? False
+
+"""
