c280e41e00bb1a58a98683d832e11e07351d68b1
Omid Hashemzadeh <omidh2015@gmail.com>
2025-06-19 15:27:45 +0100

    improve Plus One

--- LeetCodeExamples/66. Plus One.py
+++ LeetCodeExamples/66. Plus One.py
@@ -1,3 +1,12 @@
+# class Solution(object):
+#     def plusOne(self, digits):
+#         """
+#         :type digits: List[int]
+#         :rtype: List[int]
+#         """
+
+#         plus_one : int = int(''.join(list(map(str, digits)))) + 1
+#         return [int(digit) for digit in str(plus_one)]
 
 
 class Solution(object):
@@ -6,10 +15,18 @@ class Solution(object):
         :type digits: List[int]
         :rtype: List[int]
         """
+        for i in range(len(digits)-1, -1, -1):
+            if digits[i] == 9:
+                digits[i] = 0
+            else:
+                digits[i] = digits[i] + 1
+                return digits
+        #  e.g: 999 ->  If all digits are 9, prepend 1 to the list
+        return [1] + digits
+    
 
-        plus_one : int = int(''.join(list(map(str, digits)))) + 1
-        return [int(digit) for digit in str(plus_one)]
-
-
-
-print(Solution().plusOne([4,3,2,1]))
+print(Solution().plusOne([4,3,2,1]))    # 4 3 2 2
+print(Solution().plusOne([2,9,9]))  # 300
+print(Solution().plusOne([9, 9 , 9 ,9]))  # 300
+print(Solution().plusOne([9]))  # 10
+print(Solution().plusOne([0]))  # 1
